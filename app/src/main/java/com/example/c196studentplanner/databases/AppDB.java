2431c6bc9bed0324b5845ca3c252e3081b34a182
matischneider082@gmail.com <matischneider082@gmail.com>
2020-09-27 14:19:30 -0400

    Converters

--- app/src/main/java/com/example/c196studentplanner/databases/AppDB.java
+++ app/src/main/java/com/example/c196studentplanner/databases/AppDB.java
@@ -1,5 +1,40 @@
 package com.example.c196studentplanner.databases;
 
-public abstract class AppDB {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.c196studentplanner.models.Assessment;
+import com.example.c196studentplanner.models.Course;
+import com.example.c196studentplanner.models.Mentor;
+import com.example.c196studentplanner.models.Note;
+import com.example.c196studentplanner.models.Term;
+
+@Database(entities = {Term.class, Course.class, Assessment.class, Note.class, Mentor.class}, exportSchema = false, version = 1)
+// @TypeConverters(Converters.class)
+public abstract class AppDB extends RoomDatabase {
+    private static final String DB_NAME = "app_db";
+    private static AppDB instance;
+
+    public static synchronized AppDB getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), AppDB.class, DB_NAME).allowMainThreadQueries().build();
+        }
+        return instance;
+    }
+
+    // Dao Calls
+    public abstract TermDao termDao();
+
+    public abstract CourseDao courseDao();
+
+    public abstract AssessmentDao assessmentDao();
+
+    public abstract NoteDao noteDao();
+
+    public abstract MentorDao mentorDao();
+
 
 }
