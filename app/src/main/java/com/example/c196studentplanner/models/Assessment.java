e7240b2e25823b34c7b80759340ef682e54a763d
matischneider082@gmail.com <matischneider082@gmail.com>
2020-09-27 08:17:53 -0400

    Added Assessment & Notes

--- app/src/main/java/com/example/c196studentplanner/models/Assessment.java
+++ app/src/main/java/com/example/c196studentplanner/models/Assessment.java
@@ -30,38 +30,37 @@ public class Assessment {
     public int getAssessment_id() {
         return assessment_id;
     }
-
-    // Setters
-    public void setAssessment_id(int assessment_id) {
-        this.assessment_id = assessment_id;
-    }
-
     public int getCourse_id_fk() {
         return course_id_fk;
-    }
-
-    public void setCourse_id_fk(int course_id_fk) {
-        this.course_id_fk = course_id_fk;
     }
 
     public String getAssessment_title() {
         return assessment_title;
     }
 
-    public void setAssessment_title(String assessment_title) {
-        this.assessment_title = assessment_title;
-    }
-
     public String getAssessment_type() {
         return assessment_type;
     }
 
-    public void setAssessment_type(String assessment_type) {
-        this.assessment_type = assessment_type;
-    }
-
     public Date getDue_date() {
         return due_date;
+    }
+
+    // Setters
+    public void setAssessment_id(int assessment_id) {
+        this.assessment_id = assessment_id;
+    }
+
+    public void setCourse_id_fk(int course_id_fk) {
+        this.course_id_fk = course_id_fk;
+    }
+
+    public void setAssessment_title(String assessment_title) {
+        this.assessment_title = assessment_title;
+    }
+
+    public void setAssessment_type(String assessment_type) {
+        this.assessment_type = assessment_type;
     }
 
     public void setDue_date(Date due_date) {
--- app/src/main/java/com/example/c196studentplanner/models/Note.java
+++ app/src/main/java/com/example/c196studentplanner/models/Note.java
@@ -1,4 +1,57 @@
 package com.example.c196studentplanner.models;
 
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+import static androidx.room.ForeignKey.CASCADE;
+
+@Entity(tableName = "note_table",
+        foreignKeys = @ForeignKey(entity = Course.class, parentColumns = "course_id", childColumns = "course_id_fk",
+                onDelete = CASCADE))
 public class Note {
+    @PrimaryKey(autoGenerate = true)
+    private int note_id;
+    @ColumnInfo(name = "course_id_fk")
+    private int course_id_fk;
+    @ColumnInfo(name = "note_title")
+    private String note_title;
+    @ColumnInfo(name = "note_text")
+    private String note_text;
+
+    // Getters
+    public int getNote_id() {
+        return note_id;
+    }
+
+    // Setters
+    public void setNote_id(int note_id) {
+        this.note_id = note_id;
+    }
+
+    public int getCourse_id_fk() {
+        return course_id_fk;
+    }
+
+    public void setCourse_id_fk(int course_id_fk) {
+        this.course_id_fk = course_id_fk;
+    }
+
+    public String getNote_title() {
+        return note_title;
+    }
+
+    public void setNote_title(String note_title) {
+        this.note_title = note_title;
+    }
+
+    public String getNote_text() {
+        return note_text;
+    }
+
+    public void setNote_text(String note_text) {
+        this.note_text = note_text;
+    }
 }
