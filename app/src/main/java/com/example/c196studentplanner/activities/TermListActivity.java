0287f33e3d0ef7f388a78297ecb911715e6192b0
matischneider082@gmail.com <matischneider082@gmail.com>
2020-09-28 10:53:56 -0400

    Populate Data Addition

---
+++ app/src/main/java/com/example/c196studentplanner/activities/TermListActivity.java
@@ -0,0 +1,31 @@
+package com.example.c196studentplanner.activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.c196studentplanner.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class TermListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/c196studentplanner/databases/PopulateDatabase.java
+++ app/src/main/java/com/example/c196studentplanner/databases/PopulateDatabase.java
@@ -11,6 +11,9 @@ import com.example.c196studentplanner.models.Mentor;
 import com.example.c196studentplanner.models.Note;
 import com.example.c196studentplanner.models.Term;
 
+import java.util.Calendar;
+import java.util.List;
+
 public class PopulateDatabase extends AppCompatActivity {
     public static String LOG_TAG = "PopData";
     Term tempTerm1 = new Term();
@@ -50,18 +53,125 @@ public class PopulateDatabase extends AppCompatActivity {
     }
 
     private void insertNotes() {
+        List<Course> courseList = appDB.courseDao().getCourseList();
+        if (courseList == null) return;
+
+        tempNote1.setNote_title("Note 1");
+        tempNote1.setNote_text("New Note for Course");
+        tempNote1.setCourse_id_fk(courseList.get(0).getCourse_id());
+
+        tempNote2.setNote_title("Note 2");
+        tempNote2.setNote_text("New Note for Course");
+        tempNote2.setCourse_id_fk(courseList.get(0).getCourse_id());
+
+        tempNote3.setNote_title("Note 1");
+        tempNote3.setNote_text("New Note for Course");
+        tempNote3.setCourse_id_fk(courseList.get(1).getCourse_id());
+
+        appDB.noteDao().insertAll(tempNote1, tempNote2, tempNote3);
+
+
     }
 
     private void insertMentors() {
+        List<Course> courseList = appDB.courseDao().getCourseList();
+        if (courseList == null) return;
+
+        tempMentor1.setMentor_name("Bob Bobert");
+        tempMentor1.setMentor_phone("666-6666");
+        tempMentor1.setMentor_email("bob@bobert.org");
+        tempMentor1.setCourse_id_fk(courseList.get(0).getCourse_id());
+
+        tempMentor2.setMentor_name("Boberta Bobert");
+        tempMentor2.setMentor_phone("646-6666");
+        tempMentor2.setMentor_email("boberta@bobert.org");
+        tempMentor2.setCourse_id_fk(courseList.get(1).getCourse_id());
+
+        tempMentor3.setMentor_name("Bobertino Boberts");
+        tempMentor3.setMentor_phone("648-1116");
+        tempMentor3.setMentor_email("bobertino@boberts.org");
+        tempMentor3.setCourse_id_fk(courseList.get(2).getCourse_id());
+
+        appDB.mentorDao().insertAll(tempMentor1, tempMentor2, tempMentor3);
     }
 
     private void insertAssessments() {
     }
 
     private void insertCourses() {
+        Calendar start;
+        Calendar end;
+        List<Term> termList = appDB.termDao().getTermList();
+        if (termList == null) return;
+
+        start = Calendar.getInstance();
+        end = Calendar.getInstance();
+        start.add(Calendar.MONTH, -2);
+        end.add(Calendar.MONTH, 1);
+        tempCourse1.setCourse_name("C196 - Android App");
+        tempCourse1.setCourse_start(start.getTime());
+        tempCourse1.setCourse_end(end.getTime());
+        tempCourse1.setCourse_notes("Notes for C196");
+        tempCourse1.setCourse_status("Active");
+        tempCourse1.setTerm_id_fk(termList.get(0).getTerm_id());
+
+
+        start = Calendar.getInstance();
+        end = Calendar.getInstance();
+        start.add(Calendar.MONTH, -2);
+        end.add(Calendar.MONTH, 1);
+        tempCourse2.setCourse_name("C154 -  Capstone");
+        tempCourse2.setCourse_start(start.getTime());
+        tempCourse2.setCourse_end(end.getTime());
+        tempCourse2.setCourse_notes("capstone notes");
+        tempCourse2.setCourse_status("Active");
+        tempCourse2.setTerm_id_fk(termList.get(01).getTerm_id());
+
+
+        start = Calendar.getInstance();
+        end = Calendar.getInstance();
+        start.add(Calendar.MONTH, -2);
+        end.add(Calendar.MONTH, 1);
+        tempCourse3.setCourse_name("C154 -  Capstone");
+        tempCourse3.setCourse_start(start.getTime());
+        tempCourse3.setCourse_end(end.getTime());
+        tempCourse3.setCourse_notes("capstone notes");
+        tempCourse3.setCourse_status("Active");
+        tempCourse3.setTerm_id_fk(termList.get(0).getTerm_id());
+
+
     }
 
     private void insertTerms() {
+        Calendar start;
+        Calendar end;
+
+        start = Calendar.getInstance();
+        end = Calendar.getInstance();
+        start.add(Calendar.MONTH, -2);
+        end.add(Calendar.MONTH, 1);
+        tempTerm1.setTerm_name("Fall 2020");
+        tempTerm1.setTerm_start(start.getTime());
+        tempTerm1.setTerm_end(end.getTime());
+
+        start = Calendar.getInstance();
+        end = Calendar.getInstance();
+        start.add(Calendar.MONTH, 3);
+        end.add(Calendar.MONTH, 6);
+        tempTerm2.setTerm_name("Spring 2021");
+        tempTerm2.setTerm_start(start.getTime());
+        tempTerm2.setTerm_end(end.getTime());
+
+        start = Calendar.getInstance();
+        end = Calendar.getInstance();
+        start.add(Calendar.MONTH, 7);
+        end.add(Calendar.MONTH, 9);
+        tempTerm3.setTerm_name("Summer 2021");
+        tempTerm3.setTerm_start(start.getTime());
+        tempTerm3.setTerm_end(end.getTime());
+
+        appDB.termDao().insertAll(tempTerm1, tempTerm2, tempTerm3);
+
     }
 
 }
